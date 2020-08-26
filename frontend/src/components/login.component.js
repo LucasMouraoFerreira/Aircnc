8aa1898c3711dd422316c078bc0d5272df692e64
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-08-25 21:54:25 -0300

    feat(frontend): tela de login de usuário

---
+++ frontend/src/components/login.component.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    axios
+      .post("http://localhost:5000/auth", user)
+      .then((res) => {
+        localStorage.setItem("tokenAircnc", res.data.token);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    this.setState({
+      username: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="landing">
+          <div className="home-wrap">
+            <div className="home-inner"></div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row wrapper-form">
+            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+              <div className="text-center mb-2">
+                <img
+                  src={require("../images/logo-aircnc.png")}
+                  alt="Logoascalcalcmalcmlamcalmclsamclamclamsclaclma"
+                />
+              </div>
+              <div className="card card-signin">
+                <div className="card-body">
+                  <form className="form-signin" onSubmit={this.onSubmit}>
+                    <div className="form-label-group">
+                      <input
+                        type="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="E-mail"
+                        required
+                        autoFocus
+                        onChange={this.onChangeEmail}
+                        value={this.email}
+                      />
+                    </div>
+                    <div className="form-label-group">
+                      <input
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        placeholder="Senha"
+                        required
+                        onChange={this.onChangePassword}
+                        value={this.password}
+                      />
+                    </div>
+                    <button
+                      className="btn btn-lg btn-dark btn-block text-uppercase"
+                      type="submit"
+                    >
+                      Acessar
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
