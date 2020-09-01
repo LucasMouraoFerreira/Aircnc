34987216ef3fa15ab9a839729661c9e9376719db
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-09-01 20:49:12 -0300

    feat(frontend): Finalização PlaceList - lista lugares e implementa 3 filtros de busca

--- frontend/src/components/place-list.component.js
+++ frontend/src/components/place-list.component.js
@@ -1,24 +1,42 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Place = (props) => (
-  <tr>
-    <td>{props.place.name}</td>
-    <td>{props.place.price}</td>
-    <td>{props.place.location}</td>
-    <td>{props.place.category}</td>
-    <td>
-      <Link to={"/" + props.exercise._id}>Detalhes</Link>
-    </td>
-  </tr>
+  <div class="card m-2">
+    <img class="card-img-top" src={props.place.image} alt="Card " />
+    <div class="card-body">
+      <h5 class="card-title text-center font-weight-bold">
+        {props.place.name}
+      </h5>
+      <h5 class="card-text font-weight-bold text-success">
+        R$ {props.place.price}
+      </h5>
+      <p class="card-text text-justify">{props.place.description}</p>
+    </div>
+    <div class="card-footer text-center">
+      <Link
+        class="btn btn-lg btn-success font-weight-bold"
+        to={"/" + props.key}
+      >
+        Detalhes
+      </Link>
+    </div>
+  </div>
 );
 
 export default class PlaceList extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { search: null, places: [] };
+    this.state = {
+      priceFilter: 100,
+      locationFilter: "Belo Horizonte",
+      nameFilter: null,
+      places: [],
+    };
   }
 
   componentDidMount() {
@@ -33,25 +51,63 @@ export default class PlaceList extends Component {
   }
 
   placeList() {
-    return this.state.places.map((place) => {
-      return <Place place={place} key={place._id} />;
-    });
+    return this.state.places
+      .filter(
+        (place) =>
+          place.price <= this.state.priceFilter &&
+          place.name
+            .toLowerCase()
+            .includes(this.state.nameFilter.toLowerCase()) &&
+          place.location === this.state.locationFilter
+      )
+      .map((place) => {
+        return <Place place={place} key={place._id} />;
+      });
   }
-
-  searchPlace = (event) => {
-    let keyword = event.target.value;
-    this.setState({ search: keyword });
-  };
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          placeholder="Enter item to be searched"
-          onChange={(e) => this.searchSpace(e)}
-        />
-        {this.placeList()}
+        <div className="text-center inline">
+          Filtros
+          <FontAwesomeIcon icon={faFilter} size="2x" />
+        </div>
+        <div class="form-inline">
+          <input
+            type="text"
+            class="form-control mb-2 mr-sm-2"
+            placeholder="Nome"
+            onChange={(e) => {
+              this.setState({ nameFilter: e.target.value });
+            }}
+            value={this.state.nameFilter}
+          />
+          <input
+            type="text"
+            class="form-control mb-2 mr-sm-2"
+            placeholder="Preço máximo"
+            onChange={(e) => {
+              this.setState({ priceFilter: e.target.value });
+            }}
+            value={this.state.priceFilter}
+          />
+          <select
+            class="custom-select mb-2 mr-sm-2"
+            onChange={(e) => {
+              this.setState({ locationFilter: e.target.value });
+            }}
+            value={this.state.locationFilter}
+          >
+            <option value="Belo Horizonte">Belo Horizonte</option>
+            <option value="São Paulo">São Paulo</option>
+            <option value="Rio de Janeiro">Rio de Janeiro</option>
+          </select>
+        </div>
+        <div className="row">
+          <div className="class-deck col-sm-12 col-md-6 col-lg-3">
+            {this.placeList()}
+          </div>
+        </div>
       </div>
     );
   }
