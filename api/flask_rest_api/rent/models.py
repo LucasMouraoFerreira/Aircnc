0e774dcd21622e3fe5362a99230db3fa3aae10bf
Bruna <42246566+brunacmelo@users.noreply.github.com>
2020-09-08 19:03:23 -0300

    Add files via upload

---
+++ api/flask_rest_api/rent/models.py
@@ -0,0 +1,11 @@
+from flask_rest_api import db, ma
+
+class Rent(db.Model):
+	__tablename__:"rents"
+
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	place_id = db.Column(db.Integer, db.ForeignKey('place.id'))
+
+	def __repr__(self):
+        return '<Rent %s>' % self.Rent.id
