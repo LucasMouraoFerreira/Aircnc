80a8095de5ea72d7e30c3bf83c6741e5c95e6b68
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-09-12 22:44:34 -0300

    feat(frontend): middleware de autenticação JWT

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
 
 import Login from "./components/login.component";
 import Register from "./components/register.component";
@@ -11,6 +12,40 @@ import PlaceRent from "./components/rent.component";
 import User from "./components/user.component";
 
 function App() {
+  axios.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("tokenAircnc");
+
+      console.log(token);
+
+      if (
+        token &&
+        (!config.url.endsWith("login") || !config.url.endsWith("register"))
+      ) {
+        config.headers.authorization = "Bearer " + token;
+
+        console.log(config.headers.authorization);
+      }
+
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+  axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      if (error.response.status === 401 || error.response.status === 403) {
+        window.location = "/login";
+      }
+      return Promise.reject(error);
+    }
+  );
   return (
     <Router>
       <Route
