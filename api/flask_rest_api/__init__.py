1dacab3c845ed46a3ce85114f32b9c39bfa1efda
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2020-09-15 19:22:11 -0300

    adição do flask_cors na API
    Será necessário rodar o comando "pip install -r requirements.txt" para atualizar as dependências da API

--- api/flask_rest_api/__init__.py
+++ api/flask_rest_api/__init__.py
@@ -3,11 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'w8oyUPlywjAAN51OXBdfJUZ8icsRCCP7'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/exemplo_flask_rest_api'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 api = Api(app)
